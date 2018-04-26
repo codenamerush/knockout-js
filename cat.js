391cde6bbe90583ed50b80154947883433582686
Aarushi <aarushisharda@gmail.com>
2018-04-26 18:13:28 +0530

    using the with binding to make html simpler

--- cat.js
+++ cat.js
@@ -1,20 +1,29 @@
-var ViewModel = function() {
+var Cat = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('tabby.jpeg');
 	this.nicknames = ko.observableArray(['meow', 'tab', 'tbone']);
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	}
 
 	this.level = ko.computed(function() {
 		if(this.clickCount()<10)
 			return "newborn";
 		else if (this.clickCount()<50)
 			return "infant";
-		else return "teen";
+		else if (this.clickCount()<100)
+			return "Child";
+		else if (this.clickCount()<200)
+			return "Teen";
+		else return "Adult";
     }, this);
-
 }
+
+
+var ViewModel = function() {
+
+	this.currentCat = ko.observable(new Cat());
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	};
+};
 
 ko.applyBindings(new ViewModel());
